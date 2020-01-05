0e97de4921a3fea34079c75054665fbe355897e2
jeongkimin <48354469+jeongkimin@users.noreply.github.com>
2020-01-05 12:14:57 +0900

    mdn.backward debug

--- stochastic/DiagonalMixtureDensityNetwork/MixtureDensityNetwork.h
+++ stochastic/DiagonalMixtureDensityNetwork/MixtureDensityNetwork.h
@@ -40,6 +40,51 @@ public:
 		in_port = list[0]->in_port;
 	}
 
+	void sampling(Darr& x, Darr& y) {
+		static double denom = 0.0;
+		static double det = 0.0;
+		static double mahala = 0.0;
+		static double u = 0.0;
+		static double cum = 0.0;
+		charge(x, 0);
+		forward(nn_out, 0);
+		denom = 0.0;
+		for (int k = 0; k < n_group; k++) {
+			mixing_coeff[k] = exp(nn_out[k]);
+			denom += mixing_coeff[k];
+		}
+		for (int k = 0; k < n_group; k++) {
+			mixing_coeff[k] /= denom;
+		}
+
+		double likelihood_of_y = 0.0;
+
+		for (int k = 0; k < n_group; k++) {
+			det = 1.0;
+			mahala = 0.0;
+			for (int i = 0; i < n_dim; i++) {
+				mean[k][i] = nn_out[n_group + k * n_dim + i];
+				stddev[k][i] = exp(nn_out[n_group + n_group * n_dim + k * n_dim + i]);
+				det *= stddev[k][i] * stddev[k][i];
+				mahala += (y[i] - mean[k][i]) * (y[i] - mean[k][i]) / (stddev[k][i] * stddev[k][i]);
+			}
+			conditional[k] = lh_constant * pow(det, -0.5) * exp(-0.5 * mahala);
+			likelihood_of_y += mixing_coeff[k] * conditional[k];
+		}
+
+		u = km::U(rEngine);
+		cum = 0.0;
+		for (int k = 0; k < n_group; k++) {
+			cum += mixing_coeff[k];
+			if(cum >= u) {
+				for (int d = 0; d < n_dim; d++) {
+					y[d] = km::STD(rEngine) * stddev[k][d] + mean[k][d];
+				}
+				break;
+			}
+		}
+	}
+
 	double fit(Darr& x, Darr& y) {
 
 		static double denom = 0.0;
@@ -72,14 +117,165 @@ public:
 		}
 
 		for (int k = 0; k < n_group; k++) {
-			nn_out_grad[k] = mixing_coeff[k] * (1.0 - (conditional[k] / likelihood_of_y));
+			nn_out_grad[k] = mixing_coeff[k] - (mixing_coeff[k] * conditional[k] / likelihood_of_y);
 			for (int i = 0; i < n_dim; i++) {
-				nn_out_grad[n_group + k * n_dim + i] =  (conditional[k] * (mean[k][i] - y[i]))/ (stddev[k][i] * likelihood_of_y);
+				nn_out_grad[n_group + k * n_dim + i] =  (conditional[k] * mixing_coeff[k] 
+					* (mean[k][i] - y[i]))/ (stddev[k][i] * stddev[k][i] * likelihood_of_y);
 				nn_out_grad[n_group + n_group * n_dim + k * n_dim + i] 
 					= (1.0 - ((mean[k][i] - y[i]) / stddev[k][i]) * ((mean[k][i] - y[i]) / stddev[k][i])) * 
-					(conditional[k] /( likelihood_of_y*stddev[k][i]));
+					((conditional[k] * mixing_coeff[k])/( likelihood_of_y));
 			}
 		}
+
+		backward(nn_out_grad, 0);
+		return likelihood_of_y;
+	}
+};
+
+
+class DiagCovMDN2 : public Sequential {
+public:
+
+	int n_group = 0; int n_dim = 0;
+	Darr nn_out = nullptr;
+	Darr mixing_coeff = nullptr;
+	DarrList mean = nullptr;
+	DarrList stddev = nullptr;
+
+	Darr nn_out_grad = nullptr;
+	Darr conditional = nullptr;
+
+	double lh_constant = 0;
+	void setMDN(const int& n_group, const int& n_dim) {
+		this->n_group = n_group;
+		this->n_dim = n_dim;
+		lh_constant = pow(2.0 * 3.141592, -0.5 * n_dim);
+		nn_out = alloc(n_group * n_dim * 2 + n_group);
+		mixing_coeff = alloc(n_group);
+		mean = alloc(n_group, n_dim);
+		stddev = alloc(n_group, n_dim);
+		nn_out_grad = alloc(n_group * n_dim * 2 + n_group);
+		conditional = alloc(n_group);
+	}
+	~DiagCovMDN2() {
+		free(nn_out);
+		free(mixing_coeff);
+		free(mean, n_group);
+		free(stddev, n_group);
+		free(nn_out_grad);
+		free(conditional);
+	}
+	virtual void setCache(const int& cache_size) {
+		for (int i = 0; i < sequential_len; i++) {
+			list[i]->setCache(cache_size);
+		}
+		in_port = list[0]->in_port;
+	}
+
+	void sampling(Darr& x, Darr& y) {
+		static double denom = 0.0;
+		static double det = 0.0;
+		static double mahala = 0.0;
+		static double u = 0.0;
+		static double cum = 0.0;
+		charge(x, 0);
+		forward(nn_out, 0);
+		denom = 0.0;
+		for (int k = 0; k < n_group; k++) {
+			mixing_coeff[k] = exp(nn_out[k]);
+			denom += mixing_coeff[k];
+		}
+		for (int k = 0; k < n_group; k++) {
+			mixing_coeff[k] /= denom;
+		}
+
+		double likelihood_of_y = 0.0;
+
+		for (int k = 0; k < n_group; k++) {
+			det = 1.0;
+			mahala = 0.0;
+			for (int i = 0; i < n_dim; i++) {
+				mean[k][i] = nn_out[n_group + k * n_dim + i];
+				if (nn_out[n_group + n_group * n_dim + k * n_dim + i] > 0) {
+					stddev[k][i] = nn_out[n_group + n_group * n_dim + k * n_dim + i] + 1.0;
+				}
+				else {
+					stddev[k][i] = exp(nn_out[n_group + n_group * n_dim + k * n_dim + i]);
+				}
+				det *= stddev[k][i] * stddev[k][i];
+				mahala += (y[i] - mean[k][i]) * (y[i] - mean[k][i]) / (stddev[k][i] * stddev[k][i]);
+			}
+			conditional[k] = lh_constant * pow(det, -0.5) * exp(-0.5 * mahala);
+			likelihood_of_y += mixing_coeff[k] * conditional[k];
+		}
+
+		u = km::U(rEngine);
+		cum = 0.0;
+		for (int k = 0; k < n_group; k++) {
+			cum += mixing_coeff[k];
+			if (cum >= u) {
+				for (int d = 0; d < n_dim; d++) {
+					y[d] = km::STD(rEngine) * stddev[k][d] + mean[k][d];
+				}
+				break;
+			}
+		}
+	}
+
+	double fit(Darr& x, Darr& y) {
+
+		static double denom = 0.0;
+		static double det = 0.0;
+		static double mahala = 0.0;
+		charge(x, 0);
+		forward(nn_out, 0);
+		denom = 0.0;
+		for (int k = 0; k < n_group; k++) {
+			mixing_coeff[k] = exp(nn_out[k]);
+			denom += mixing_coeff[k];
+		}
+		for (int k = 0; k < n_group; k++) {
+			mixing_coeff[k] /= denom;
+		}
+
+		double likelihood_of_y = 0.0;
+
+		for (int k = 0; k < n_group; k++) {
+			det = 1.0;
+			mahala = 0.0;
+			for (int i = 0; i < n_dim; i++) {
+				mean[k][i] = nn_out[n_group + k * n_dim + i];
+				if (nn_out[n_group + n_group * n_dim + k * n_dim + i] > 0) {
+					stddev[k][i] = nn_out[n_group + n_group * n_dim + k * n_dim + i] + 1.0;
+				}
+				else {
+					stddev[k][i] = exp(nn_out[n_group + n_group * n_dim + k * n_dim + i]);
+				}
+				det *= stddev[k][i] * stddev[k][i];
+				mahala += (y[i] - mean[k][i]) * (y[i] - mean[k][i]) / (stddev[k][i] * stddev[k][i]);
+			}
+			conditional[k] = lh_constant * pow(det, -0.5) * exp(-0.5 * mahala);
+			likelihood_of_y += mixing_coeff[k] * conditional[k];
+		}
+
+		for (int k = 0; k < n_group; k++) {
+			nn_out_grad[k] = mixing_coeff[k] - (mixing_coeff[k] * conditional[k] / likelihood_of_y);
+			for (int i = 0; i < n_dim; i++) {
+				if (nn_out[n_group + n_group * n_dim + k * n_dim + i] > 0) {
+					nn_out_grad[n_group + n_group * n_dim + k * n_dim + i]
+						= (1.0 - ((mean[k][i] - y[i]) / stddev[k][i]) * ((mean[k][i] - y[i]) / stddev[k][i])) *
+						((conditional[k] * mixing_coeff[k]) / (likelihood_of_y * stddev[k][i]));
+				}
+				else {
+					nn_out_grad[n_group + n_group * n_dim + k * n_dim + i]
+						= (1.0 - ((mean[k][i] - y[i]) / stddev[k][i]) * ((mean[k][i] - y[i]) / stddev[k][i])) *
+						((conditional[k] * mixing_coeff[k]) / (likelihood_of_y));
+				}
+				nn_out_grad[n_group + k * n_dim + i] = (conditional[k] * mixing_coeff[k]
+					* (mean[k][i] - y[i])) / (stddev[k][i] * stddev[k][i] * likelihood_of_y);
+			}
+		}
+
 		backward(nn_out_grad, 0);
 		return likelihood_of_y;
 	}
--- stochastic/DiagonalMixtureDensityNetwork/main.cpp
+++ stochastic/DiagonalMixtureDensityNetwork/main.cpp
@@ -2,35 +2,35 @@
 #include "Optimizer.h"
 #include "Viewer.h"
 int main() {
-	DiagCovMDN mdn; {
-		mdn.push(new Dense(1, 8));
+	DiagCovMDN2 mdn; {
+		mdn.push(new Dense(1, 4));	
+		mdn.push(new ReLU(4));
+		mdn.push(new Dense(4, 8));
 		mdn.push(new ReLU(8));
-		mdn.push(new Dense(8, 8));
-		mdn.push(new ReLU(8));
-		mdn.push(new Dense(8, 8));
-		mdn.push(new ReLU(8));
-		mdn.push(new Dense(8, 8));
-		mdn.push(new ReLU(8));
-		mdn.push(new Dense(8, 2 + 2 + 2));
+		mdn.push(new Dense(8, 12));
+		mdn.push(new ReLU(12));
+		mdn.push(new Dense(12, 16));	
+		mdn.push(new ReLU(16));
+		mdn.push(new Dense(16, 18));	
+		mdn.push(new ReLU(18));
+		mdn.push(new Dense(18, 2 + 2 + 2));
 	}	mdn.setMDN(2, 1);	mdn.setCache(1);
 
 	Optimizer optim(mdn);
-	optim.setLearningRate(0.001);
+	optim.setLearningRate(0.0005);
 
 	int n_data = 60000;
 	DarrList x = alloc(n_data, 1);
 	DarrList y = alloc(n_data, 1);
 
-
-	for (int ep = 0; ep < 100; ep++) {
+	for (int ep = 0; ep < 300; ep++) {
 		for (int n = 0; n < n_data; n += 2) {
-			x[n][0] = km::U(rEngine) * 4.0 - 2.0;
-			y[n][0] = sin(3.0 * x[n][0]) + km::STD(rEngine) * 0.2 * sin(2.0 * x[n + 1][0]);
+			x[n][0] = km::U(rEngine) * 2.0 - 1.0;
+			y[n][0] = 0.3 * sin(8.0 * x[n][0]) -0.5 + km::STD(rEngine) * 0.2 * sin(9.0 * x[n][0]);
 
-			x[n + 1][0] = km::U(rEngine) * 4.0 - 2.0;
-			y[n + 1][0] = cos(5.0 * x[n + 1][0]) + km::STD(rEngine) * 0.1 * cos(x[n + 1][0]);
+			x[n + 1][0] = km::U(rEngine) * 2.0 - 1.0;
+			y[n + 1][0] = 0.5 * cos(8.0 * x[n + 1][0]) + 0.5 + km::STD(rEngine) * 0.05 * cos(8.0 * x[n+1][0]);
 		}
-
 		double lh = 0.0;
 
 		for (int i = 0; i < n_data; i++) {
@@ -40,5 +40,10 @@ int main() {
 		}
 		cout << lh / double(n_data) << endl;
 	}
-
+	for (int n = 0; n < n_data; n++) {
+		x[n][0] = km::U(rEngine) * 2.0 - 1.0;
+		mdn.sampling(x[n], y[n]);
+	}
+	Viewer plt(x, y, n_data);
+	plt.run();
 }
